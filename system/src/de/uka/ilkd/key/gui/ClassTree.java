EmpiricallyConfused/key

commit 2d10608369f23fc1ceba819d2c7a5eb7d26a409f

Compress linear paths in the proof management tree view.
Expanding paths like de.uka.ilkd.key.gui every time was a pain.

system/src/de/uka/ilkd/key/gui/ClassTree.java
@@ -235,6 +235,27 @@ public class ClassTree extends JTree {
             }
         }
     }
+
+
+    private static void compressLinearPaths(DefaultMutableTreeNode root) {
+
+        int numChildren = root.getChildCount();
+        for(int i = 0; i < numChildren; i++) {
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode)root.getChildAt(i);
+            int numGrandChildren = child.getChildCount();
+            if (numGrandChildren == 1) {
+                DefaultMutableTreeNode grandChild = (DefaultMutableTreeNode)child.getFirstChild();
+                child.removeFromParent();
+                root.add(grandChild);
+                Entry e1 = (Entry) child.getUserObject();
+                Entry e2 = (Entry) grandChild.getUserObject();
+                e2.string = e1.string + "." + e2.string;
+                compressLinearPaths(root);
+            }
+        }
+    }
+
+
     
     /**
      * <p>
@@ -310,6 +331,7 @@ public class ClassTree extends JTree {
             insertIntoTree(rootNode, kjtsarr[i], addContractTargets, services);
         }
         
+        compressLinearPaths(rootNode);
         return rootNode;
     }
     
@@ -401,7 +423,7 @@ public class ClassTree extends JTree {
     //-------------------------------------------------------------------------    
     
     static class Entry {
-        public final String string;
+        public String string;
         public KeYJavaType kjt = null;
         public IObserverFunction target = null;
         public int numMembers = 0;

commit f01e934deda8947026ed317dcff1a1e457dfb31d

Fixed loading of examples.
Broken in 2d10608369f23fc1ceba819d2c7a5eb7d26a409f.

system/src/de/uka/ilkd/key/gui/ClassTree.java
@@ -245,6 +245,8 @@ public class ClassTree extends JTree {
             int numGrandChildren = child.getChildCount();
             if (numGrandChildren == 1) {
                 DefaultMutableTreeNode grandChild = (DefaultMutableTreeNode)child.getFirstChild();
+                //stop compressing at method name
+                if (((Entry)grandChild.getUserObject()).target != null) continue;
                 child.removeFromParent();
                 root.add(grandChild);
                 Entry e1 = (Entry) child.getUserObject();
@@ -346,27 +348,32 @@ public class ClassTree extends JTree {
         DefaultMutableTreeNode node 
                 = (DefaultMutableTreeNode) getModel().getRoot();
         assert node != null;        
+        pathVector.add(node);
         do {
-            //save current node
-            pathVector.add(node);
-            
-            //get next part of the name
-            int lastIndex = index;
-            index = fullClassName.indexOf(".", ++index);
-            if(index == -1) {
-                index = length;
+            if (fullClassName.startsWith(".")) {
+                fullClassName = fullClassName.substring(1);
             }
-            String namePart = fullClassName.substring(lastIndex + 1, index);
-            
-            //get child node, go down to it
-            DefaultMutableTreeNode childNode = getChildByString(node, namePart);
-	    assert childNode != null : "type not found: " + namePart 
-	                               + " (part of " + fullClassName
-	                               + ", for observer " + target + ")";
+            //get next part of the name
+            DefaultMutableTreeNode childNode = null;
+            for (int i=0; i<node.getChildCount(); i++) {
+                childNode = (DefaultMutableTreeNode) node.getChildAt(i);
+                Entry e = (Entry) childNode.getUserObject();
+                if (fullClassName.startsWith(e.string)) {
+                    pathVector.add(childNode);
+                    fullClassName = fullClassName.substring(e.string.length());
+                    break;
+                } else childNode = null;
+            }
+	    if (childNode == null) {
+                System.err.println(
+                    "Cannot find ("+kjt.getFullName()+","+target + ") in dialog. "+
+                    "Unmatched fragment \""+fullClassName+"\"");
+                return;
+            }
             node = childNode;
-        } while(index != length);
-        TreePath incompletePath = new TreePath(pathVector.toArray());
-        TreePath path = incompletePath.pathByAddingChild(node);
+        } while(fullClassName.length() > 0);
+        TreePath path = new TreePath(pathVector.toArray());
+        TreePath incompletePath = null;
         
         //extend tree path to method
         if(target != null) {
@@ -374,6 +381,8 @@ public class ClassTree extends JTree {
             if(methodNode != null) {
         	incompletePath = path;            
         	path = path.pathByAddingChild(methodNode);
+            } else {
+                incompletePath = path.getParentPath();
             }
         }
         

system/src/de/uka/ilkd/key/gui/ExampleChooser.java
@@ -260,7 +260,6 @@ public final class ExampleChooser extends JDialog {
 	exampleList = new JTree();
 	exampleList.setModel(model);
 	exampleList.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-	exampleList.setRootVisible(false);
 	exampleList.addTreeSelectionListener(
 	        new TreeSelectionListener() {
 	            @Override